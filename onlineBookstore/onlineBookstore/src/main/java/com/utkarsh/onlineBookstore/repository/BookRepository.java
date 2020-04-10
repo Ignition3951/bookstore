5cdf743b91544c71bda49df790ba55f8ba0901a9
91885 <91885@192.168.1.7>
2020-04-11 00:51:49 +0530

    Completed the angular feature 1 front end part.

--- onlineBookstore/onlineBookstore/src/main/java/com/utkarsh/onlineBookstore/repository/BookRepository.java
+++ onlineBookstore/onlineBookstore/src/main/java/com/utkarsh/onlineBookstore/repository/BookRepository.java
@@ -5,10 +5,12 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.utkarsh.onlineBookstore.model.Book;
 
 @Repository
+@CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
