3c0bad73e57272219a4964d27b26d7d1bc386fb1
91885 <91885@192.168.1.7>
2020-04-12 11:01:51 +0530

    Completed the feature 3 which is search by category id.

--- onlineBookstore/onlineBookstore/src/main/java/com/utkarsh/onlineBookstore/repository/BookRepository.java
+++ onlineBookstore/onlineBookstore/src/main/java/com/utkarsh/onlineBookstore/repository/BookRepository.java
@@ -3,7 +3,11 @@ package com.utkarsh.onlineBookstore.repository;
 import java.math.BigInteger;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -13,4 +17,6 @@ import com.utkarsh.onlineBookstore.model.Book;
 @CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long> {
 
+	@RestResource(path = "categoryid")
+	Page<Book> findByCategoryId(@Param("id") Long id,Pageable pageable);
 }
